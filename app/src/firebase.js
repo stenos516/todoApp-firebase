9360054fa12e8df1a2ff4b7177871305d0aa3d51
Stefano.L <83345164+stenos516@users.noreply.github.com>
2024-11-06 17:54:22 +0100

    Update firebase.js

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore} from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnjXJ_6cOeJf7i0yYhWumMYt8nlth67A4",
-  authDomain: "minions-pern-app.firebaseapp.com",
-  projectId: "minions-pern-app",
-  storageBucket: "minions-pern-app.firebasestorage.app",
-  messagingSenderId: "640137560914",
-  appId: "1:640137560914:web:35fe8ef0a92b570fea308b",
-  measurementId: "G-PWEGBVG6YC"
+  
 };
 
 const app = initializeApp(firebaseConfig);
